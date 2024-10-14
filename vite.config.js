8957c9f82082a5874430e1f795db5551892f1846
Ogunderu Noah <ifeoluwadayonoah.com>
2024-10-14 20:41:43 +0100

    config-3

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,7 +10,4 @@ export default defineConfig({
       svgrOptions: {}
     }),
   ],
-  server: {
-    port: 5173
-  },
 })
